ecf0fe84d939b2f6f6f1540f477844531def1a8a
Sven Lehnert <mail@sven-lehnert.de>
2014-06-17 12:21:48 +0200

    new version 1.0.2

--- loader.php
+++ loader.php
@@ -3,7 +3,7 @@
  Plugin Name: BuddyForms Attach Posts to Groups Extension
  Plugin URI: http://themekraft.com
  Description: Create engaged communities with every post.
- Version: 1.0.1
+ Version: 1.0.2
  Author: Sven Lehnert
  Author URI: http://themekraft.com
  Licence: GPLv3
@@ -27,22 +27,22 @@
  ****************************************************************************
  */
 
-define('buddyforms-Group-Extension', '0.1');
+define('BuddyForms-Attach-Posts-to-Groups-sExtension', '1.0.2');
 
 /**
- * Loads buddyforms-Group-Extension files only if BuddyPress is present
+ * Loads BuddyForms Attach Posts to Groups Extension files only if BuddyPress is present
  *
- * @package BuddyPress Custom Group Types
+ * @package BuddyForms Attach Posts to Groups Extension
  * @since 0.1-beta
  */
-function buddyforms_Group_Extension_init() {
+function BuddyForms_Attach_Posts_to_Groups_Extension_init() {
 	global $wpdb;
 
 	if (is_multisite() && BP_ROOT_BLOG != $wpdb->blogid)
 		return;
 
 	require (dirname(__FILE__) . '/buddyforms-groups.php');
-	$buddyforms_Group_Extension = new BuddyForms_Group_Extension();
+	$BuddyForms_Attach_Posts_to_Groups_Extension_init = new BuddyForms_Group_Extension();
 }
 
-add_action('bp_loaded', 'buddyforms_Group_Extension_init', 0);
+add_action('bp_loaded', 'BuddyForms_Attach_Posts_to_Groups_Extension_init', 0);
--- readme.txt
+++ readme.txt
@@ -3,7 +3,7 @@ Contributors: svenl77
 Tags: buddypress, user, members, profiles, custom post types, taxonomy, frontend posting, frontend editing, groups, post attached to groups
 Requires at least: WordPress 3.x, BuddyPress 1.7.x
 Tested up to: WordPress 3.9, BuddyPress 2.x
-Stable tag: 1.0.1
+Stable tag: 1.0.2
 License: GPLv2 or later
 License URI: http://www.gnu.org/licenses/gpl-2.0.html
 
@@ -71,6 +71,10 @@ BuddyForms gives you these possibilities for a wide variety of uses.
 coming soon
 
 == Changelog ==
+
+= 1.0.2 =
+Add a new option to redirect a group to the attached post
+Fixed a url rewrite issue
 
 = 1.0.1 =
 * clean up code
